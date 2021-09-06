27e1944d96d11e0fc4c5fba8dac295924b5f3f63
Har15h <saiharishmanne@gmail.com>
2021-09-07 00:52:51 +0530

    Added joystick, characters and implemented MVC for player and enemy

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController
+{
+    public GuardController(GuardView guardView)
+    {
+
+    }
+
+    public Vector3 Movement(Vector3 playerPos, Vector3 targetPos)
+    {
+        Vector3 pos = Vector3.MoveTowards(playerPos, targetPos, Time.deltaTime);
+        return pos;
+    }
+}
